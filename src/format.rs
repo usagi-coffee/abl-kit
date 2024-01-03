72324e02961e9f1a2f7de9bd11cdcd525e2803fa
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2024-01-03 22:58:56 +0100

    kit: add simple uppercase style fixer

--- src/format.rs
+++ src/format.rs
@@ -1,37 +1,7 @@
-use crate::parser;
-
-use tree_sitter::TreeCursor;
 use std::path::Path;
 
-struct State {
-    indentation_level: usize,
-    indentations: Vec<usize>,
-}
-
-const TAB_SPACES: usize = 2;
-
-const INDENTATED_STATEMENTS: [&'static str; 18] = [
-    "if_statement",
-    "else_if_statement",
-    "else_statement",
-    "loop_statement",
-    "do_statement",
-    "repeat_statement",
-    "function_statement",
-    "procedure_statement",
-    "transaction_statement",
-    "for_statement",
-    "find_statement",
-    "case_statement",
-    "assign_statement",
-    "catch_statement",
-    "finally_statement",
-    "abl_statement",
-    "do_block",
-    "include"
-];
-
-const EXTENDED_STATEMENTS: [&'static str; 2] = ["else_statement", "else_if_statement"];
+mod reindent;
+mod style;
 
 pub fn fix_file(file: &String) {
     let source: String;
@@ -43,89 +13,9 @@ pub fn fix_file(file: &String) {
         source = std::fs::read_to_string(wd.join(file).as_path()).unwrap();
     }
 
-    let output = fix(&source);
+    let mut output;
+    output = reindent::transform(&source);
+    output = style::transform(&output);
+
     std::fs::write(file, output).expect("Failed to write to file");
-}
-
-pub fn fix(source: &String) -> String {
-    let mut parser = parser::setup();
-
-    let tree = parser.parse(&source, None).unwrap();
-
-    let mut state = State {
-        indentation_level: 0,
-        indentations: Vec::new(),
-    };
-
-    // Fill all lines as zero indentation
-    for _ in source.lines() {
-        state.indentations.push(0);
-    }
-
-    let mut cursor = tree.walk();
-
-    traverse_tree(&mut cursor, &mut state);
-
-    let mut output = String::new();
-    let mut was_empty = false;
-
-    for (index, line) in source.lines().enumerate() {
-        if was_empty && line.trim().is_empty() {
-            continue;
-        }
-
-        output.push_str(&" ".repeat(state.indentations[index] * TAB_SPACES));
-        output.push_str(line.trim());
-        output.push_str("\n");
-
-        was_empty = line.trim().is_empty();
-    }
-
-    if output.len() < 1 {
-        panic!("Output is empty! Not writing to file");
-    }
-
-    output
-}
-
-fn traverse_tree(cursor: &mut TreeCursor, state: &mut State) {
-    for node in cursor.node().children(cursor) {
-        let start = node.start_position().row;
-        let mut end = node.end_position().row;
-
-        if INDENTATED_STATEMENTS.contains(&node.kind()) {
-            if node.kind() == "abl_statement" && start == end {
-                continue;
-            }
-
-            // Find terminator
-            let mut cursor = node.walk();
-            for node in node.children(&mut cursor) {
-                if node.kind() == "END" {
-                    end = node.end_position().row;
-                }
-            }
-
-            let mut root = state.indentation_level.clone();
-            if !EXTENDED_STATEMENTS.contains(&node.kind()) {
-                state.indentation_level += 1;
-            }
-
-            for i in start + 1..end {
-                state.indentations[i] = state.indentation_level;
-            }
-
-            traverse_tree(&mut node.walk(), state);
-
-            if EXTENDED_STATEMENTS.contains(&node.kind()) {
-                root -= 1;
-            } else {
-                state.indentation_level -= 1;
-            }
-
-            state.indentations[end] = root;
-        } else {
-            traverse_tree(&mut node.walk(), state);
-        }
-    }
 }
---
+++ src/format/style.rs
@@ -0,0 +1,50 @@
+use crate::parser;
+use tree_sitter::{InputEdit, TreeCursor};
+
+const KEYWORDS: &'static [u8] = include_bytes!("../../data/keywords.txt");
+
+// Could be refactored into macro but whatever
+fn keywords() -> Vec<&'static str> {
+    std::str::from_utf8(KEYWORDS).unwrap().lines().collect()
+}
+
+pub fn transform(source: &String) -> String {
+    let mut parser = parser::setup();
+
+    let mut output = source.clone();
+
+    let tree = parser.parse(&output, None).unwrap();
+    let mut cursor = tree.walk();
+
+    traverse_tree(&mut cursor, &mut output);
+    output
+}
+
+fn traverse_tree(cursor: &mut TreeCursor, source: &mut String) {
+    for mut node in cursor.node().children(cursor) {
+        if keywords().contains(&node.kind()) {
+            let range = node.range();
+
+            source.replace_range(
+                range.start_byte..range.end_byte,
+                source[range.start_byte..range.end_byte]
+                    .to_uppercase()
+                    .as_str(),
+            );
+
+            let edit = InputEdit {
+                start_byte: range.start_byte,
+                old_end_byte: range.end_byte,
+                new_end_byte: range.start_byte + &node.kind().len() + 4,
+                start_position: range.start_point,
+                old_end_position: range.end_point,
+                new_end_position: node.end_position(),
+            };
+
+            node.edit(&edit);
+        }
+
+        traverse_tree(&mut node.walk(), source);
+    }
+}
+
